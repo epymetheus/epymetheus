0494e4c8fd1ae9f5eafc5e17be2c386e31bd569c
Shota Imaki <shota.imaki.0801@gmail.com>
2021-06-03 21:39:27 +0900

    ENH: Enable Strategy to dump and load trades as JSON format (close #40)

--- epymetheus/_utils.py
+++ epymetheus/_utils.py
@@ -1,3 +1,8 @@
+import json
+
+import numpy as np
+
+
 def print_if_verbose(*objects, verbose=True, **kwargs):
     """
     Print objects to the text stream if `verbose=True``.
@@ -5,3 +10,23 @@ def print_if_verbose(*objects, verbose=True, **kwargs):
     """
     if verbose:
         print(*objects, **kwargs)
+
+
+class _JSONEncoderNumpy(json.JSONEncoder):
+    """
+    JSON encoder that can seriealize numpy objects.
+    """
+
+    def default(self, o):
+        if isinstance(o, np.integer):
+            return int(o)
+        if isinstance(o, np.floating):
+            return float(o)
+        elif isinstance(o, np.ndarray):
+            return o.tolist()
+        else:
+            return super().default(o)
+
+
+def to_json(o) -> str:
+    return _JSONEncoderNumpy().encode(o)
